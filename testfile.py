600cfae684ba47e49d7f28210c615413f6f27c31
JINAH-CHU <jinah.chu@gmail.com>
2021-10-15 15:50:32 +0900

    another test

--- testfile.py
+++ testfile.py
@@ -11,11 +11,11 @@ def get_target_price(ticker, k):
     target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
     return target_price
 
-# def get_start_time(ticker):
-#     """시작 시간 조회"""
-#     df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
-#     start_time = df.index[0]
-#     return start_time
+def get_start_time(ticker):
+    """시작 시간 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
+    start_time = df.index[0]
+    return start_time
 
 # def get_balance(ticker):
 #     """잔고 조회"""
@@ -40,29 +40,44 @@ print("autotrade start")
 while True:
     try:
         now = datetime.datetime.now()
-        print(now)
-        #start_time = get_start_time("KRW-AXS") # 9시
+        print("현재시각",now)
+        start_time = get_start_time("KRW-AXS") # 9시
         #print(start_time)
-        end_time = now + datetime.timedelta(hours=1)
-        print(end_time)
+        end_time = start_time + datetime.timedelta(hours=31)
+        print("종료시각",end_time)
 
         # if 9시부터 다음날 8시 50분 50초
         if now < end_time:
-            target_price = get_target_price("KRW-AXS", 0.5)
-            print(target_price)
-            current_price= pyupbit.get_current_price("KRW-AXS")
-            print(current_price)
+            axs_target_price = get_target_price("KRW-AXS", 0.5)
+            xrp_target_price = get_target_price("KRW-XRP", 0.5)
+            
+            axs_current_price= pyupbit.get_current_price("KRW-AXS")
+            xrp_current_price= pyupbit.get_current_price("KRW-XRP")
+
+            print("KRW-AXS target:", axs_target_price)
+            print("KRW-AXS current:", axs_current_price)
+            print("KRW-XRP target:", xrp_target_price)
+            print("KRW-XRP current:", xrp_current_price)
             # current_price = get_current_price("KRW-AXS")
-            if target_price < current_price:
-                krw = upbit.get_balance("KRW")
+
+            krw = upbit.get_balance("KRW")
+            print("KRW in my account", krw)
+            if axs_target_price < axs_current_price:
                 if krw > 5000:
                     upbit.buy_market_order("KRW-AXS", 10000)#krw*0.9995) #수수료
-        # 나머지 10초동안 대략 5000원 이상, 수수료 제외하고는 다 매도. 
+                    print("AXS in my account:", upbit.get_balance("AXS"))
+            if xrp_target_price < xrp_current_price:
+                if krw > 5000:
+                    upbit.buy_market_order("KRW-XRP", 10000)     
+                    print("XRP in my account:",upbit.get_balance("XRP"))   
         else:
             axs = upbit.get_balance("AXS")
+            xrp = upbit.get_balance("XRP")
             if axs > 0.025:
                 upbit.sell_market_order("KRW-AXS", axs*0.9995)
-        time.sleep(1)
+            if xrp > 4:
+                upbit.sell_market_order("KRW-XRP", xrp*0.9995)
+        time.sleep(10)
     except Exception as e:
         print(e)
         time.sleep(1)
